9ec31beee34065d1c4ef8f46f75b7142f7ede93f
Elpanchez <santiagosanchez.ribero@gmail.com>
2025-04-05 12:51:54 -0500

    feat: Fix

--- gamma.py
+++ gamma.py
@@ -1,6 +1,6 @@
 
 print("TATIS")
-print("0_o Santiago Sanchez Ribero se droga")
+print("0_o Tatis se droga")
 print("Que mas dani loco")
 print("Holaaaaa amigos de youtube :P") 
 print("Santiago Sanchez Ribero")
